6d492472e95d96527a2270f3a9375fe1aba08ade
rahul2214 <dyrahulnaik22@gmail.com>
2025-01-19 10:30:24 +0530

    Resolved merge conflicts

--- frontend/src/skeletonLoader.jsx
+++ frontend/src/skeletonLoader.jsx
@@ -8,6 +8,9 @@ const SkeletonLoader = () => (
         <Skeleton variant="rectangular" width={400} height={150} style={{ marginBottom: '16px' }} />
         <Skeleton variant="text" width="60%" />
         <Skeleton variant="text" width="80%" />
+        <Skeleton variant="rectangular" width={400} height={150} style={{ marginBottom: '16px' }} />
+        <Skeleton variant="text" width="60%" />
+        <Skeleton variant="text" width="80%" />
     </div>
 );
 
